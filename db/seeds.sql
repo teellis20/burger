2835f2b45671ccf3e54f94698d9230477aced983
Tellis <teellis20@gmail.com>
2019-10-29 19:48:30 -0700

    initial push

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon BBQ Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Hawaiian Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Mushroom Burger", false);
